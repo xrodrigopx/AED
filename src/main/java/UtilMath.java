6b12cbb5d7591f6fefbc242e48367e4c13df5360
Rodrigo Perdomo <roperdomo047@gmail.com>
2025-03-22 11:23:39 -0300

    First Commit

--- src/main/java/UtilMath.java
+++ src/main/java/UtilMath.java
@@ -9,23 +9,25 @@ public static int factorial(int num)
 
 el factorial de 5 es 5 x 4 x 3 x 2 x 1.
 Esto quiere decir que el factorial de 5 es: 5 * (5-1) * (5-2) * (5-3) * (5-4) y ahi termina.
-Por lo tanto, vamos a definir tres variables, una para nuestro numero, una para el producto de la multiplicacion y una para el resultado de la resta.
-La resta tiene que ser el numero menos un contador incremental de 1 en 1 que haremos con un for.
-Luego, con un if, vamos a evaluar si la resta, si la resta no es igual a 1, realizamos la multiplicacion y guardamos el valor. Con cada iteracion, debemos multiplicar el resultado anterior por el que ya tenemos. Si la resta es igual a uno, se imprime el numero resultante. Para 5 es 720.
-
-
+Por lo tanto, vamos a tener que definir un metodo que contenga la logica del calculo del factorial.
+Este metodo tiene un bucle for que se encargara de realizar las operaciones aritmeticas necesarias.
+El for, seria uno que comienza en el numero dado, le reste uno y nunca llegue a 0. Con cada iteracion el resultado es multiplicado por el numero dado. Devuelve un resultado.
  */
 
 public class UtilMath {
     public static void main(String[] args) {
-        // variables
-        int aNumber = 5; // queremos el factorial de esto.
-        int bNumber = 0; // resultado del producto.
-        int cNumber = 0; // resultado de la resta.
-
-        for (int i = 0; i < aNumber; i++) {
-            System.out.println(bNumber);
+        int aNumber = 5; // variable de numero cualquiera
+        int factorialResult = factorial(aNumber); // metodo que se le aplica al numero deseado.
+        System.out.println(factorialResult); // imprimir resultado
+    }
+    // ahora vamos a definir el metodo para calcular el factorial
+    public static int factorial (int num){
+        int resultado = num; // vamos a almacenar el resultado en una variable
+        for (int i = num -1; i > 0; i--){ // esta logica comienza con el valor de la variable, decrementa en 1, hasta llegar a 1
+            resultado *= i; // con cada iteracion se debe multiplicar el resultado de la iteracion anterior por el numero dado
+            System.out.println(resultado); // mensaje para debbugear
         }
-
+        return resultado; // devolver resultado del metodo.
     }
 }
+
