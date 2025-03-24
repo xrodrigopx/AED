f7d0b3d0710a1fb1a3af21b2f6436624c69b55f0
Rodrigo Perdomo <rperdomo@ucu.edu.uy>
2025-03-24 10:57:28 -0300

    ahora si

--- src/main/java/org/example/Libros.java
+++ src/main/java/org/example/Libros.java
@@ -1,4 +1,23 @@
 package org.example;
 
+import java.util.Stack;
+
 public class Libros {
+    public static void main(String[] args) {
+        Stack<String> libros = new Stack<String>();
+        libros.push("Java");
+        libros.push("Python");
+        libros.push("JavaScript");
+        libros.push("PHP");
+
+
+        System.out.println(libros); // muestra toda la pila
+
+        System.out.println(libros.peek()); // mmuestra el libro que esta mas arriba
+        System.out.println(libros.pop()); // elimina el elemento de mas arriba
+        System.out.println(libros);
+        System.out.println(libros.peek()); // ahora Python deberia estar en lo mas alto.
+        System.out.println(libros.search("Java")); // esto muestra la posicion del libro con respecto al mas alto.
+        System.out.println(libros.isEmpty()); // consulta si la pila esta vacia.
+    }
 }
