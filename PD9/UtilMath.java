7ac2e425208cf6a2e0b781b00c069c37b34cb67c
rperdomo <roperdomo047@gmail.com>
2025-04-02 13:42:54 -0300

    PD10

--- PD9/UtilMath.java
+++ PD9/UtilMath.java
@@ -1,19 +1,3 @@
-package org.example;/*
-Implementar un método que calcule el factorial de un número entero cualquiera, siendo este un
-parámetro del método. Utilizar únicamente bucles for.
-Si N=3 entonces factorial(N) = 3.2.1 = 6
-Una posible firma del método:
-public static int factorial(int num)
-{...}
-
-
-el factorial de 5 es 5 x 4 x 3 x 2 x 1.
-Esto quiere decir que el factorial de 5 es: 5 * (5-1) * (5-2) * (5-3) * (5-4) y ahi termina.
-Por lo tanto, vamos a tener que definir un metodo que contenga la logica del calculo del factorial.
-Este metodo tiene un bucle for que se encargara de realizar las operaciones aritmeticas necesarias.
-El for, seria uno que comienza en el numero dado, le reste uno y nunca llegue a 0. Con cada iteracion el resultado es multiplicado por el numero dado. Devuelve un resultado.
- */
-
 public class UtilMath {
     public static void main(String[] args) {
         int aNumber = 5; // variable de numero cualquiera
