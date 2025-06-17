37c9c67499dcc4db7e741c4ebb639fb9ed45f256
Rodrigo Perdomo <roperdomo047@gmail.com>
2025-06-16 21:35:27 -0300

    arreglar package com.mycompany.mavenproject1;

---
+++ UT8/PD/PD1/mavenproject1/src/main/java/com/mycompany/mavenproject1/IAdyacencia.java
@@ -0,0 +1,20 @@
+package com.mycompany.mavenproject1;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ernesto
+ */
+public interface IAdyacencia {
+
+    double getCosto();
+
+    TVertice getDestino();
+
+    Comparable getEtiqueta();
+    
+}
