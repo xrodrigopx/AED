25591161699151070aa512766f4ac68ce4a5f828
Rodrigo Perdomo <roperdomo047@gmail.com>
2025-06-19 18:56:41 -0300

    hola

---
+++ UT9/TAs/UT9-TA1/src/TClasificador.java
@@ -0,0 +1,117 @@
+
+public class TClasificador {
+	public static final int METODO_CLASIFICACION_INSERCION = 1;
+	public static final int METODO_CLASIFICACION_SHELL = 2;
+	public static final int METODO_CLASIFICACION_BURBUJA = 3;
+
+	/**
+	 * Punto de entrada al clasificador
+	 * 
+	 * @param metodoClasificacion
+	 * @param orden
+	 * @param tamanioVector
+	 * @return Un vector del tam. solicitado, ordenado por el algoritmo solicitado
+	 */
+	public int[] clasificar(int[] datosParaClasificar, int metodoClasificacion) {
+		switch (metodoClasificacion) {
+		case METODO_CLASIFICACION_INSERCION:
+			return ordenarPorInsercion(datosParaClasificar);
+		case METODO_CLASIFICACION_SHELL:
+			return ordenarPorShell(datosParaClasificar);
+		case METODO_CLASIFICACION_BURBUJA:
+			return ordenarPorBurbuja(datosParaClasificar);
+		default:
+			System.err.println("Este codigo no deberia haberse ejecutado");
+			break;
+		}
+		return datosParaClasificar;
+	}
+
+	private void intercambiar(int[] vector, int pos1, int pos2) {
+		int temp = vector[pos2];
+		vector[pos2] = vector[pos1];
+		vector[pos1] = temp;
+	}
+
+
+	/**
+	 * @param datosParaClasificar
+	 * @return
+	 */
+	private int[] ordenarPorShell(int[] datosParaClasificar) {
+		int j, inc;
+		int[] incrementos = new int[] { 3223, 358, 51, 10, 3, 1 };
+
+		for (int posIncrementoActual = 1; posIncrementoActual < incrementos.length; posIncrementoActual++) {
+			inc = incrementos[posIncrementoActual];
+			if (inc < (datosParaClasificar.length / 2)) {
+				for (int i = inc; i < datosParaClasificar.length; i++) {
+					j = i - inc;
+					while (j >= 0) {
+						if (datosParaClasificar[j] > datosParaClasificar[j + inc]) {
+							intercambiar(datosParaClasificar, j, j + inc);
+							j = j--;
+						} 
+					}
+				}
+			}
+		}
+		return datosParaClasificar;
+	}
+
+
+	/**
+	 * @param datosParaClasificar
+	 * @return
+	 */
+	protected int[] ordenarPorInsercion(int[] datosParaClasificar) {
+		if (datosParaClasificar != null) {
+			for (int i = 2; i < datosParaClasificar.length; i++) {
+				int j = i - 1;
+				while ((j >= 0) && (datosParaClasificar[j+1] > datosParaClasificar[j])) {
+					intercambiar(datosParaClasificar, j, j + 1);
+					j--;
+				}
+			}
+			return datosParaClasificar;
+		}
+		return null;
+	}
+
+	private int[] ordenarPorBurbuja(int[] datosParaClasificar) {
+		datosParaClasificar = null;
+		int n = datosParaClasificar.length - 1;
+		for (int i = 0; i <= n; i++) {
+			for (int j = n; j >= (i + 1); j--) {
+				if (datosParaClasificar[j] < datosParaClasificar[j - 1]) {
+					intercambiar(datosParaClasificar, j - 1, j);
+				}
+			}
+		}
+		return datosParaClasificar;
+	}
+
+	public static void main(String args[]) {
+		TClasificador clasif = new TClasificador();
+		GeneradorDatosGenericos gdg = new GeneradorDatosGenericos();
+		int[] vectorAleatorio = gdg.generarDatosAleatorios();
+		int[] vectorAscendente = gdg.generarDatosAscendentes();
+		int[] vectorDescendente = gdg.generarDatosDescendentes();
+
+		int[] resAleatorio = clasif.clasificar(vectorAleatorio,
+				METODO_CLASIFICACION_INSERCION);
+		for (int i = 0; i < resAleatorio.length; i++) {
+			System.out.print(resAleatorio[i] + " ");
+		}
+		int[] resAscendente = clasif.clasificar(vectorAscendente,
+				METODO_CLASIFICACION_INSERCION);
+		for (int i = 0; i < resAscendente.length; i++) {
+			System.out.print(resAscendente[i] + " ");
+		}
+		int[] resDescendente = clasif.clasificar(vectorDescendente,
+				METODO_CLASIFICACION_INSERCION);
+		for (int i = 0; i < resDescendente.length; i++) {
+			System.out.print(resDescendente[i] + " ");
+		}
+	}
+}
